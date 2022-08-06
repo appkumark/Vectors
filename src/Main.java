5708651b88543fc6e68e0dc2e35095f4fd8226c8
appkumark <appk.kanubuddi@gmail.com>
2022-08-06 16:49:11 +0530

    Vector implementation

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import vectors.Employee;
+
+import java.util.List;
+import java.util.Vector;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        List<Employee> employeeList = new Vector<>();
+        employeeList.add(new Employee("Jane", "Jones", 123));
+        employeeList.add(new Employee("John", "Doe", 4567));
+        employeeList.add(new Employee("Mary", "Smith", 22));
+        employeeList.add(new Employee("Mike", "Wilson", 3245));
+
     }
 }
